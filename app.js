de6626ab031a62e972356a5d63007d50019f4022
lincolncheesy <110572541+lincolncheesy@users.noreply.github.com>
2026-09-11 13:52:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1548,6 +1548,8 @@ export function renderApp() {
 }
 
 // Initial Boot
-window.addEventListener('DOMContentLoaded', () => {
+if (document.readyState === 'loading') {
+  window.addEventListener('DOMContentLoaded', () => renderApp());
+} else {
   renderApp();
-});
+}
